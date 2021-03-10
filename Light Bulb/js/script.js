df9bade49936fbc9d43c03845064c979d506dd04
Vagner <vagnerferreira185@gmail.com>
2021-03-10 19:08:04 -0300

    Adicionada pasta Light bulb

---
+++ Light Bulb/js/script.js
@@ -0,0 +1,49 @@
+let ligarDesligar = document.getElementById('ligar-desligar')
+let bulb = document.getElementById('bulb')
+let body = document.querySelector('body')
+
+ligarDesligar.addEventListener('click', ligouDesligou)
+bulb.addEventListener('dblclick', quebrou)
+
+function isQuebrada() {
+    return bulb.src.indexOf('quebrada') > -1
+}
+
+function ligou() { 
+    if ( !isQuebrada() ) {
+        bulb.src = 'img/ligada.png'
+    }  
+}
+
+function desligou() { 
+    if ( !isQuebrada() ) {
+        bulb.src = 'img/desligada.png'
+    }  
+}
+
+function quebrou() {
+    if ( !isQuebrada() ) {
+        bulb.src = 'img/quebrada.png'
+    } 
+}
+
+function ligouDesligou() {
+    if (ligarDesligar.textContent == 'Turn On') {
+        ligou()
+        ligarDesligar.textContent = 'Turn Of'
+        ligarDesligar.style.background = '#ffe700'
+        body.style.background = '#feffae'
+    } else {
+        desligou()
+        ligarDesligar.textContent = 'Turn On'
+        ligarDesligar.style.background = ''
+        body.style.background = ''
+    }
+}
+
+
+
+
+
+
+
